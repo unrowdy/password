84d9d9d95d75feae693ccbe21573962fe51dd02a
Chris Palmeri <chrispalmeri@gmail.com>
2022-11-19 13:50:24 -0600

    Indentation to tabs

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
 function scale(num, len) {
-    return Math.round(num / 255 * (len - 1));
+	return Math.round(num / 255 * (len - 1));
 }
 
 var charset = {};
@@ -12,109 +12,109 @@ charset.all = charset.lowalpha + charset.upalpha + charset.digit + charset.symbo
 var prior = '';
 
 function generate(len) {
-    var needs = ['lowalpha', 'upalpha', 'digit'];
+	var needs = ['lowalpha', 'upalpha', 'digit'];
 
-    // cause it could be 0, and you  would get a missing digit in the output
-    if(charset.symbol.length > 0) {
-        needs.push('symbol');
-    }
+	// cause it could be 0, and you  would get a missing digit in the output
+	if(charset.symbol.length > 0) {
+		needs.push('symbol');
+	}
 
-    // in case there is no input
-    len = parseInt(len) || needs.length;
+	// in case there is no input
+	len = parseInt(len) || needs.length;
 
-    var password = new Array(len);
-    var indices = [...password.keys()];
+	var password = new Array(len);
+	var indices = [...password.keys()];
 
-    var random = new Uint8Array(len * 2);
-    crypto.getRandomValues(random);
-    random = [...random];
+	var random = new Uint8Array(len * 2);
+	crypto.getRandomValues(random);
+	random = [...random];
 
-    while (indices.length > 0) {
-        var choices = 'all';
-        if(indices.length === needs.length) {
-            choices = needs.shift();
-        }
+	while (indices.length > 0) {
+		var choices = 'all';
+		if(indices.length === needs.length) {
+			choices = needs.shift();
+		}
 
-        var id = scale(random.shift(), indices.length);
-        var pos = indices.splice(id, 1)[0];
+		var id = scale(random.shift(), indices.length);
+		var pos = indices.splice(id, 1)[0];
 
-        var pick = [...charset[choices]];
-        var ix = scale(random.shift(), pick.length);
-        var val = pick[ix];
+		var pick = [...charset[choices]];
+		var ix = scale(random.shift(), pick.length);
+		var val = pick[ix];
 
-        for (var i = 0; i < needs.length; i++) {
-            if(charset[needs[i]].includes(val)) {
-                needs.splice(i, 1);
-                break;
-            }
-        }
+		for (var i = 0; i < needs.length; i++) {
+			if(charset[needs[i]].includes(val)) {
+				needs.splice(i, 1);
+				break;
+			}
+		}
 
-        password[pos] = val;
-    }
+		password[pos] = val;
+	}
 
-    return password.join('');
+	return password.join('');
 }
 
 function highlight(element) {
-    const string = element.innerText;
-    const pattern = /(\d+)|([^a-zA-Z\d]+)/g;
+	const string = element.innerText;
+	const pattern = /(\d+)|([^a-zA-Z\d]+)/g;
 
-    function replacer(match, p1, p2) {
-        if(p1 !== undefined) {
-            return `<span class="token number">${match}</span>`;
-        }
-        if (p2 !== undefined) {
-            return `<span class="token punctuation">${match}</span>`;
-        }
-    }
+	function replacer(match, p1, p2) {
+		if(p1 !== undefined) {
+			return `<span class="token number">${match}</span>`;
+		}
+		if (p2 !== undefined) {
+			return `<span class="token punctuation">${match}</span>`;
+		}
+	}
 
-    element.innerHTML = string.replace(pattern, replacer);
+	element.innerHTML = string.replace(pattern, replacer);
 }
 
 function regen() {
-    var len = parseInt(document.getElementById('length').value);
-    document.getElementById('res').textContent = generate(len);
-    highlight(document.getElementById('res'));
+	var len = parseInt(document.getElementById('length').value);
+	document.getElementById('res').textContent = generate(len);
+	highlight(document.getElementById('res'));
 }
 
 window.addEventListener('load', function() {
-    document.getElementById('allow').addEventListener('change', function() {
-        charset.symbol = this.value;
-        charset.all = charset.lowalpha + charset.upalpha + charset.digit + charset.symbol;
-        regen();
-    });
+	document.getElementById('allow').addEventListener('change', function() {
+		charset.symbol = this.value;
+		charset.all = charset.lowalpha + charset.upalpha + charset.digit + charset.symbol;
+		regen();
+	});
 
-    document.getElementById('url').addEventListener('change', function() {
-        if(this.checked) {
-            // save the old value temporarily
-            prior = document.getElementById('allow').value;
-            document.getElementById('allow').value = '-._~';
-            document.getElementById('allow').disabled = true;
-        } else {
-            document.getElementById('allow').value = prior;
-            document.getElementById('allow').disabled = false;
-        }
-        document.getElementById('allow').dispatchEvent(new Event('change'));
-        document.getElementById('url').blur();
-    });
+	document.getElementById('url').addEventListener('change', function() {
+		if(this.checked) {
+			// save the old value temporarily
+			prior = document.getElementById('allow').value;
+			document.getElementById('allow').value = '-._~';
+			document.getElementById('allow').disabled = true;
+		} else {
+			document.getElementById('allow').value = prior;
+			document.getElementById('allow').disabled = false;
+		}
+		document.getElementById('allow').dispatchEvent(new Event('change'));
+		document.getElementById('url').blur();
+	});
 
-    document.getElementById('length').addEventListener('change', function() {
-        regen();
-    });
+	document.getElementById('length').addEventListener('change', function() {
+		regen();
+	});
 
-    document.getElementById('gen').addEventListener('click', function() {
-        regen();
-        document.getElementById('gen').blur();
-    });
+	document.getElementById('gen').addEventListener('click', function() {
+		regen();
+		document.getElementById('gen').blur();
+	});
 
-    document.getElementById('copy').addEventListener('click', function() {
-        // this is a promise, but I don't care to handle it
-        navigator.clipboard.writeText(document.getElementById('res').textContent);
-        document.getElementById('copy').blur();
-    });
+	document.getElementById('copy').addEventListener('click', function() {
+		// this is a promise, but I don't care to handle it
+		navigator.clipboard.writeText(document.getElementById('res').textContent);
+		document.getElementById('copy').blur();
+	});
 
-    document.getElementById('allow').value = charset.symbol;
-    document.getElementById('url').checked = false;
-    document.getElementById('length').value = 16;
-    regen();
+	document.getElementById('allow').value = charset.symbol;
+	document.getElementById('url').checked = false;
+	document.getElementById('length').value = 16;
+	regen();
 });
